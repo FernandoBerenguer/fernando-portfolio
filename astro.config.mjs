0d1397ef80dff1fd941e0f4e405a01a131b9c11e
Fernando Berenguer <fernando.berenguer.borrego@gmail.com>
2026-05-22 19:08:26 +0200

    chore: set production site URL for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://example.com', // TODO: actualizar en la spec de deploy
+  site: 'https://fernando-berenguer.pages.dev',
   integrations: [react()],
   i18n: {
     defaultLocale: 'es',
